e977641e11dbdceb7d4e2dd04c32577e1bc999a5
soeren olischer <olischer.s@gmail.com>
2022-07-27 21:00:14 +0200

    period tests done, next habit

--- tests/test_period.py
+++ tests/test_period.py
@@ -1,4 +1,4 @@
-import pathlib, sys, pytest
+import pathlib, sys, pytest, datetime
 sys.path.append(str(pathlib.Path(__file__).parents[1]))
 from habtrack import period
 
@@ -10,7 +10,6 @@ def period_obj():
     return period.Period("monthly")
 
 ##### - __init__ test         - #####
-
 @pytest.mark.parametrize("periodicity", ["daily", "weekly", "monthly"])
 def test_period_obj(periodicity):
     """tests if a Period object is instanciated"""
@@ -22,14 +21,25 @@ def test_period_obj_defect_input(periodicity):
     with pytest.raises(TypeError):
         period(periodicity)
 
+
+
 ##### - amount_checkoffs test - #####
+def test_period_amount_checkoffs_default(period_obj):
+    """test if the default amount of checkoffs is assigned"""
+    assert period_obj.amount_checkoffs == 1
 
-##### - _set_fct test         - #####
+def test_period_amount_checkoffs_change(period_obj):
+    """test if the amount of checkoffs can be changed"""
+    period_obj.amount_checkoffs = 36
+    assert period_obj.amount_checkoffs == 36
 
-##### - _add_day test         - #####
 
-##### - _add_week test        - #####
 
-##### - _add_month test       - #####
-
-##### - __call__ test         - #####
+##### - amount_checkoffs test - #####
+@pytest.mark.parametrize("periods, start_date, end_date", [
+    ("daily", datetime.date(2000, 1, 1), datetime.date(2000, 1, 2)), 
+    ("weekly", datetime.date(2000, 1, 1), datetime.date(2000, 1, 8)),
+    ("monthly", datetime.date(2000, 1, 1), datetime.date(2000, 2, 1))])
+def test_period_set_fct_daily(periods, start_date, end_date):
+    """test functionality is correctly assigned and works intentionally"""
+    assert period.Period(periods)(start_date, 1) == end_date
