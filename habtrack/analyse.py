f224141fb86c67c3c15a99ea5b4f217e515f4040
soeren olischer <olischer.s@gmail.com>
2022-07-27 23:40:40 +0200

    get_streaks done-but wuha what a ride, next => rest of analyse

--- habtrack/analyse.py
+++ habtrack/analyse.py
@@ -1,77 +1,53 @@
-from datetime import date
-
 def get_streaks(habit):
-	def set_time_period(start):
-		return (period(start, 0), period(start, 1))
+    def set_time_period(start):
+        return (period(start, 0), period(start, 1))
 
-	def set_streak_list(checkoffs, time_period, ret_lst, amount_checkoffs):
-		added = False
-		counter = 0
-		for i in checkoffs:
-			if time_period[0] <= i <= time_period[1]:
-				counter += 1
-				ret_lst[-1].append(i)
-		
-		checkoffs = checkoffs.difference(ret_lst[-1])
+    def lst_diff(lst_1, lst_2):
+        for i in lst_2:
+            if i in lst_1:
+                lst_1.remove(i)
 
-		if counter >= amount_checkoffs: added=True
-		if not added and ret_lst[-1]: ret_lst.append(list())
+    def set_streak_list(checkoffs, time_period, ret_lst, amount_checkoffs):
+        added = False
+        counter = 0
+        for checkoff in checkoffs:
+            if time_period[0] <= checkoff <= time_period[1]:
+                counter += 1
+                ret_lst[-1].append(checkoff)
 
-		return checkoffs
+        if counter >= amount_checkoffs: added=True
+        if not added and ret_lst[-1]: ret_lst.append(list())
 
-	checkoffs = habit.checkoffs
-	period = habit.periodicity
-	creation_date = habit.creation_date
-	streaks_list = [list()]
-	time_period = set_time_period(creation_date)
-	amount_checkoffs = period.amount_checkoffs
+        lst_diff(checkoffs, ret_lst[-1])
+    
+    checkoffs = habit.checkoffs.copy()
+    period = habit.periodicity
+    creation_date = habit.creation_date
+    streaks_list = [list()]
+    time_period = set_time_period(creation_date)
+    amount_checkoffs = period.amount_checkoffs
 
-	while checkoffs:
-		checkoffs = set_streak_list(checkoffs, time_period, streaks_list, amount_checkoffs)
-		time_period = set_time_period(time_period[1])
-	return streaks_list
+    while checkoffs:
+        set_streak_list(checkoffs, time_period, streaks_list, amount_checkoffs)
+        time_period = set_time_period(time_period[1])
 
-def get_longest_streak(habit):
-	"""
-	TODO handle case when several streaks have the same len
-	"""
-	longest_streak = list()
-	streaks = get_streaks(habit)
-	for streak in streaks:
-		if len(streak) > len(longest_streak): longest_streak = streak
-	return longest_streak
+    return streaks_list
 
-def get_habits_by_period(group, period):
-	habits_with_period = list()
-	for habit in group:
-		if habit.periodicity.periodicity == period:
-			habits_with_period.append(habit)
-	return habits_with_period
+def get_longest_streak():
+    pass
 
-def is_broken(habit):
-	time_period = (habit.periodicity(date.today(), -1), habit.periodicity(date.today(), 0))
-	for checkoff in habit.checkoffs:
-		if time_period[0] < checkoff < time_period[1]:
-			return False
-	return True
+def get_habits_by_period():
+    pass
 
-def get_longest_streak_of_habits(group):
-	linked_habit = None
-	longest_streak = list()
+def is_broken():
+    pass
 
-	for habit in group:
-		if len(get_longest_streak(habit)) > len(longest_streak):
-			longest_streak = get_longest_streak(habit)
-			linked_habit = habit
+def get_longest_streak():
+    pass
 
-	return (linked_habit, longest_streak)
-
-def list_habits(group):
-	for i in group:
-		print(i)
+def list_habits():
+    pass
 
 def list_checkoffs(habit):
-	print(habit)
-	checkoffs = sorted(list(habit.checkoffs))
-	for checkoff in checkoffs:
-		print(f"  {checkoff}")
+    print(habit)
+    [print(f"  {i}") for i in sorted(habit.checkoffs)]
